402001562b3437518965741eb1438ffbc6b91606
syahrulbagas <71810571+syahrulbagas@users.noreply.github.com>
2020-12-30 02:42:46 +0700

    Add files via upload
    JS

---
+++ assets/js/scroll.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
